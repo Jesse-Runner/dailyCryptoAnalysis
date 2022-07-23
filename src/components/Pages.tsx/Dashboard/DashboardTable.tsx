c46d63497caf84db19c0fe817238150b184ddf7f
Jesse Runner <jesse.runner@coxautoinc.com>
2022-07-23 10:57:44 -0400

    pushnig up site to change computers

--- src/components/Pages.tsx/Dashboard/DashboardTable.tsx
+++ src/components/Pages.tsx/Dashboard/DashboardTable.tsx
@@ -3,55 +3,120 @@ import { InlineStylesModel } from "../../../models/InlineStyleModel";
 
 const styles: InlineStylesModel = {
   coinImage: {
-    height: 25,
-    width: 25,
+    height: 20,
+    width: 20,
+    marginLeft: ".5vw",
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  coinNameCol: {
+    fontWeight: 600,
   },
   headerRow: {
-    color: "white",
-    marginLeft: ".5vw",
+    color: "rgb(146,153,170)",
+    // marginLeft: ".5vw",
+    backgroundColor: "rgb(22,24,29)",
+    // paddingTop: "5px",
+    paddingBottom: "5px",
+    fontSize: 16,
   },
-  individualRow: {
+  headerRowTop: {
+    color: "rgb(146,153,170)",
+    backgroundColor: "rgb(22,24,29)",
+    fontSize: 16,
+  },
+  headerPadding: {
+    paddingLeft: ".5vw",
+  },
+  individualRowEven: {
     color: "white",
-    marginLeft: ".5vw",
+  },
+  individualRowOdd: {
+    color: "white",
+    // marginLeft: ".5vw",
+    backgroundColor: "rgb(22,24,29)",
   },
   tabledMainContainer: {
-    border: ".5px solid rgba(164,164,164,.35)",
-    width: "40vw",
+    // border: ".5px solid rgba(164,164,164,.35)",
+    // borderRadius: 25,
+    width: "70vw",
     marginLeft: "calc(20vw - 239px)",
+    // padding: "10px 10px",
   },
 };
 
 export const DashboardTable = ({ coinData }: any) => {
+  const headerRowTop = (
+    <Row style={styles.headerRowTop}>
+      <Col style={styles.headerPadding} span={3}></Col>
+      <Col span={2}></Col>
+      <Col span={3}></Col>
+      <Col span={2}></Col>
+      <Col span={6} style={{ textAlign: "center", fontSize: 14 }}>
+        UDPI Risk
+      </Col>
+      <Col span={2} style={{ fontSize: 14 }}>
+        Mom. Bias
+      </Col>
+      <Col span={4} style={{ textAlign: "center", fontSize: 14 }}>
+        Trend Confidence
+      </Col>
+      <Col span={2} style={{ fontSize: 14 }}>
+        MDC
+      </Col>
+    </Row>
+  );
+
   const headerRow = (
     <Row style={styles.headerRow}>
-      <Col span={4}>Asset</Col>
-      <Col span={3}>Price (USD)</Col>
+      <Col style={styles.headerPadding} span={3}>
+        Asset
+      </Col>
+      <Col span={2}>Price (USD)</Col>
       <Col span={3}>Price (SATS)</Col>
-      <Col span={3}>Market Cap</Col>
-      <Col span={3}>Mock one</Col>
-      <Col span={3}>Mock two</Col>
-      <Col span={3}>Mock Three</Col>
+      <Col span={2}>MCap</Col>
+      <Col span={2}>Short</Col>
+      <Col span={2}>Medium</Col>
+      <Col span={2}>Long</Col>
+      <Col span={2}>MBI</Col>
+      <Col span={2}>TCI</Col>
+      <Col span={2}>Crit Level</Col>
+      <Col span={2}>Crit Level</Col>
     </Row>
   );
   const individualRow = () => {
     const rows: any = [];
 
     if (coinData !== null && coinData !== undefined) {
-      const top25 = coinData.slice(0, 24);
+      const top25 = coinData.slice(0, 19);
 
-      top25.forEach((coin: any) => {
+      top25.forEach((coin: any, index: any) => {
         rows.push(
-          <Row style={styles.individualRow} gutter={8}>
-            <Col span={2}>
+          <Row
+            style={
+              index % 2 === 0
+                ? styles.individualRowEven
+                : styles.individualRowOdd
+            }
+            // gutter={8}
+            align="middle"
+          >
+            <Col span={1}>
               <img style={styles.coinImage} src={coin.image}></img>
             </Col>
-            <Col span={2}>{coin.symbol.toUpperCase()}</Col>
+            <Col span={2} style={styles.coinNameCol}>
+              {coin.symbol.toUpperCase()}
+            </Col>
+            <Col span={2}>{coin.current_price}</Col>
             <Col span={3}>{coin.current_price}</Col>
-            <Col span={3}>{coin.current_price}</Col>
-            <Col span={3}>{coin.market_cap}</Col>
-            <Col span={3}>{coin.market_cap}</Col>
-            <Col span={3}>{coin.market_cap}</Col>
-            <Col span={3}>{coin.market_cap}</Col>
+            <Col span={2}>{coin.market_cap}</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
+            <Col span={2}>1</Col>
           </Row>
         );
       });
@@ -63,6 +128,7 @@ export const DashboardTable = ({ coinData }: any) => {
 
   return (
     <div style={styles.tabledMainContainer}>
+      {headerRowTop}
       {headerRow}
       {individualRow()}
     </div>
